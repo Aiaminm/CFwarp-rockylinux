2fcdb2953b42cef263f15455091d49e80ff0248c
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2022-05-08 16:09:21 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -475,10 +475,10 @@ rm -rf /usr/local/bin/wgcf /etc/wireguard/wgcf.conf /etc/wireguard/wgcf-profile.
 ShowWGCF
 if [[ $release = Centos ]]; then
 if [[ ${vsid} =~ 8 ]]; then
-cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
-sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
-sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
+#cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
+#curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
+#sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
+#sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
 yum clean all && yum makecache
 fi
 yum install epel-release -y;yum install iproute iptables wireguard-tools -y
